d4a2e4e8430aedcbe98eb35611a9184ad66c6791
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-08-12 15:25:32 +0100

    added docstring

--- hardwaredevice.py
+++ hardwaredevice.py
@@ -9,18 +9,20 @@ class HardwareDevice(object):
     def __init__(self, mac_addr, label, nports, power_usage=0):
         self.uf_mac_addr = mac_addr
         mac_addr = ''.join(chr(int(c, 16)) for c in mac_addr.split(':'))
-        print(mac_addr)
+        # convert hexadecimal formatted MAC address to proper bytes.
+        
         self.mac_addr = mac_addr
-
-
         self.label = label
-
         self.nports = nports
-        self.ports = {-1: None}  # index: hardware
-
+        self.ports = {-1: None}  # {index: hardware}
         self.power_usage = power_usage
 
     def _connect_port(self, hardware):
+        """
+        Connects a piece of hardware to the hardware port for direct
+        hardware I/O over a standard such as 802.3 or 802.11/*
+        """
+        
         if max(self.ports.keys())+1 > self.nports:
             return False
         self.ports[max(self.ports.keys())+1] = hardware
