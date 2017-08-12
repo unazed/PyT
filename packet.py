8654350a86250d44b563fcbe36d70ba91ac345cc
Unazed Spectaculum <leethax0r.coder@gmail.com>
2017-08-12 15:29:22 +0100

    added docstring and note

--- packet.py
+++ packet.py
@@ -6,12 +6,22 @@ class Packet(object):
     def __init__(self, *args):
         self.args = args
 
-    def __str__(self):  # default string method
-        return ''.join(self.args)
-
     def __repr__(self):
+        """
+        Makes a more user-friendly representation of the repr() output or
+        when seen implicitly such as when you print the bound instance.
+        """
+        
         return "<Packet: (length=%d), (type=%s) >" %\
                 (
-                    len(self.__str__()),
-                    self.__type__()
+                    len(str(self)),
+                    type(self)
                 )
+        
+        
+# NOTE: might reconsider making a class, 
+# -this simply has 2 dunder methods and 
+# -a fairly useless concept for existence
+# -as it is only for organizing certain 
+# -packets like Ethernet packets under
+# -a common parent.
